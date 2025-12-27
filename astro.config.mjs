f010960a19dbf8c9982bebadf8ff0ce0a8617026
Alex Jimenez <ajimenez@creativoapp.com>
2025-12-27 12:06:05 -0500

    Added partytown for added tag analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,24 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   redirects: {
     '/politicas': '/politicas-de-privacidad',
     '/paginas-web': '/paginas-web-cancun',
     '/optimizacion-web': '/posicionamiento-seo-sem',
     '/software': '/desarrollo-software',
   },
-  output: 'server', // ¡Cambia de 'static' a 'server'!
-  adapter: vercel()
+
+  // ¡Cambia de 'static' a 'server'!
+  output: 'server',
+
+  adapter: vercel(),
+  integrations: [partytown()]
 });
